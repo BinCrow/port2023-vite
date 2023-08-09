073ecfc05c9ad312d758fc21caa5c82f7183c108
yongbinkwon <92711444+BinCrow@users.noreply.github.com>
2023-08-09 22:41:27 +0900

    헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,22 @@
 export function menu() {
-  console.log(menu)
-}
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header__nav");
+
+    if(headerToggle) {
+      headerToggle.addEventListener("click", () => {
+        headerNav.classList.toggle("show");
+
+        /** 삼항연산자 */
+        headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded" ,"false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+
+        /** if문 */
+        // if(headerToggle.getAttribute("aria-expanded") === "true") {
+        //   headerToggle.setAttribute("aria-expanded" ,"false")
+        // } else {
+        //   headerToggle.setAttribute("aria-expanded", "true"); 
+    // }
+      });
+    }
+  }
